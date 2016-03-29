98dd0cadea8a32319cc55e9266d8e36ca01f9463
Stuart Munro <stuart@munro.co>
2016-03-29 12:12:36 +0100

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+import time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    st = time.mktime(time.gmtime())
+    c = 0
+    while c != 50000000:
+        c += 1
+    ft = time.mktime(time.gmtime()) - st
+    return "Hello World! time: {0}".format(ft)
+
+if __name__ == "__main__":
+    app.run()
