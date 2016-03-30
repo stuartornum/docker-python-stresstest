e66e2a86defc231b9851a3a04904a1aef3e48238
Stuart Munro <stuart@munro.co>
2016-03-30 11:42:33 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 import time
+from socket import gethostname
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    st = time.mktime(time.gmtime())
-    return "Hello World! time: {0}".format(st)
+    return "Hello World! time: {0} - {1}".format(int(time.mktime(time.gmtime())), gethostname())
 
