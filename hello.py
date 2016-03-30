a9365375002969be548ebbb4ac102ca619d0b54d
Stuart Munro <stuart@munro.co>
2016-03-30 11:07:20 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     st = time.mktime(time.gmtime())
     c = 0
-    while c != 50000000:
+    while c != 10000000:
         c += 1
     ft = time.mktime(time.gmtime()) - st
     return "Hello World! time: {0}".format(ft)
